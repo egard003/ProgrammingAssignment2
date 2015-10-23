b0a7e41ad85e474dbdc62e527f2b9059d0aa7a16
egard003 <egard003@plattsburgh.edu>
2015-10-23 14:20:06 -0400

    egard003 version of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions calculate and cache the inverse of a matrix
+## x variable contains the matrix
+## i variable contains the inverse of the matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of functions:
+## set run to essentially start over with a new matrix
+    ## sets the value of x 
+    ## and also sets the value of i to NULL
+## get retrieves the value of x 
+## setinverse sets the value of i to the value passed to it
+## getinverse retrieves the value i 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) { 
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(d) i <<- d
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns a matrix that is the inverse of x. 
+## If the inverse has already been calculated, 
+   ## the cached calculation is returned
+## If the inverse has not already been calculated, 
+   ## it is calculated, cached, and returned
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  li <- x$getinverse()                ## set li to the return value of the getinverse function 
+                                         ## which is i
+  
+  if(!is.null(li)) {                  ## if li is not null (inverse already calculated), 
+    message("getting cached data")    ## output message and
+     return(li)                       ## return li
+  }
+  
+  data <- x$get()                     ## set data to the return value of the get function 
+                                         ## which is m
+  
+  li <- solve(data, ...)              ## set li to the inverse of data 
+  
+  x$setinverse(li)                    ## use setinverse function to cache the value of li 
+                                        ## in the i variable
+  
+  li                                  ## return li   
 }
